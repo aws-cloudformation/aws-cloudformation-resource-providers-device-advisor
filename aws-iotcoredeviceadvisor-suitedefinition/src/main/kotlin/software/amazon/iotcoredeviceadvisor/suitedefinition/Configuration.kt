ebf5795f6faad25a37be58710e92e51114bd7640
Seth Askew <askthoma@amazon.com>
2021-05-11 10:07:03 -0700

    Adding in liscence information to each kotlin file

--- aws-iotcoredeviceadvisor-suitedefinition/src/main/kotlin/software/amazon/iotcoredeviceadvisor/suitedefinition/Configuration.kt
+++ aws-iotcoredeviceadvisor-suitedefinition/src/main/kotlin/software/amazon/iotcoredeviceadvisor/suitedefinition/Configuration.kt
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package software.amazon.iotcoredeviceadvisor.suitedefinition;
 
 internal class Configuration : BaseConfiguration("aws-iotcoredeviceadvisor-suitedefinition.json")
